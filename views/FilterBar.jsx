2f20195655ff1263cf0eddf6f61d5c86dd92a63e
Vasya Boeva <vs99516@gmail.com>
2022-05-26 15:28:38 +0300

    FilterBar (not done yet)

---
+++ views/FilterBar.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+
+module.exports = function FilmBar({location}) {
+  return (
+
+  <form action='/filter' method='post' id="FilterForm"> 
+   <p><select name="locationFilter">
+{location.map((el)=> <option  value={el.id}>{el.title}</option>)}
+   </select>
+   <input  type="submit" value="Отправить"></input></p> 
+  </form>
+    
+  )
+}
